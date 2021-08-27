88e05e4d83e1ebe95bf813a9eaed8dcf603e53cf
Martin <martinaeroengg@gmail.com>
2021-08-27 19:47:09 +0530

    runtime

--- website/urls.py
+++ website/urls.py
@@ -1,16 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from django.views.static import serve
-from django.conf.urls import url
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tracker.urls')),
     path('', include('accounts.urls')),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
 urlpatterns += [
