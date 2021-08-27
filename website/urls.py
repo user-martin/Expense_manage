a256adba28fcad60594d8a4a5f6b568e77e49e37
Martin <martinaeroengg@gmail.com>
2021-08-27 19:08:32 +0530

    equirement

--- website/urls.py
+++ website/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('', include('tracker.urls')),
     path('', include('accounts.urls')),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
