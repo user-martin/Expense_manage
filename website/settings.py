7ee608d97056f0276ed8ac8931c65b1230195a57
Martin <martinaeroengg@gmail.com>
2021-08-27 17:58:40 +0530

    server_name

--- website/settings.py
+++ website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(pwnf+znz^!914bc%wg8)wj+c!_7y8-71ya65k(79gpiw8ye+2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','sampledomain.com']
+ALLOWED_HOSTS = ['127.0.0.1','familyzbudget.herokuapp.com']
 
 #Primary
 
